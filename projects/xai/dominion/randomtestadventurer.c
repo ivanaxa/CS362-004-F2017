840c29fff6f2e26a06ae64734ac5e6071eae4f13
Ivan Austin Xa <ivanaustinxa@gmail.com>
2017-11-01 22:50:39 -0700

    Assignment 4 completed

--- projects/xai/dominion/randomtestadventurer.c
+++ projects/xai/dominion/randomtestadventurer.c
@@ -1,94 +1,100 @@
 #include "dominion.h"
-#include "rngs.h"
+#include "dominion_helpers.h"
+#include <string.h>
 #include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
 #include <math.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <time.h>
 
-#define MAX_TESTS 1200
+int testAdventurer(struct gameState *, int); //function declaration
 
-// This randomly tests village
+int main(){
+    int j;
+    for (j = 0; j < 500; j++){
+    //int seed = 1000;
+    int seed = rand() % 1000 + 1; //1 through 1000
+    //int numPlayer = 3;
+    int numPlayer = rand() % 3 + 2; // 2,3,4 players
+    int r, result;
+    //int handPos = 2;
+    int handPos = 0;
+    int k[10] = {adventurer, council_room, feast, gardens, mine,
+                remodel, smithy, village, baron, great_hall};
+    struct gameState G;
 
-/// Oracle function returns 1 if state is valid after drawing village card,
-/// 0 if state is inconsistent with expected results (deckCount should be reduced)
-int validateStateAfterVillageDraw(struct gameState * state, int player, int prevActions, int prevDeckCount) {
-	if (state->deckCount[player] > prevDeckCount && prevDeckCount != 0) {
-		printf("Test failure: deckCount should be smaller than before\n");
-        printf("deckCount before: %d   ", prevDeckCount);
-        printf("deckCount after: %d\n", state->deckCount[player]);
-		return 0;
-	}
-    else if(state->numActions != prevActions + 2) {
-        printf("Test failure: numActions should be 2 larger than before\n");
-        printf("numActions before: %d   ", prevActions);
-        printf("numActions after: %d\n", state->numActions);
-    }
-	return 1;
-}
+    memset(&G, 23, sizeof(struct gameState));   // clear the game state
+    r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
 
-int main() {
-	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
-		     sea_hag, tribute, smithy};
-	int i, players, player, handCount, deckCount, numActions, seed, handPos, coinBonus;
-	struct gameState state;
-
-    srand(time(NULL));
-
-	printf("Running Random Card Test for Village\n");
-
-	for (i = 0; i < MAX_TESTS; i++) {
-
-		players = rand() % (MAX_PLAYERS - 2) + 2;
-        seed = rand();
-		coinBonus = 0;
-		handPos = rand() % 4;
-		player = i % (players + 1);
-
-        // Initialize gamestate
-		initializeGame(players, k, seed, &state);
-
-		// Initiate valid state variables
-		state.deckCount[player] = rand() % MAX_DECK;
-		state.discardCount[player] = rand() % MAX_DECK;
-		state.handCount[player] = rand() % MAX_HAND;
-
-		// Copy state variables
-		handCount = state.handCount[player];
-		deckCount = state.deckCount[player];
-        numActions = state.numActions;
-
-        cardEffect(village, 1, 1, 1, &state, handPos, &coinBonus);
-        int result = validateStateAfterVillageDraw(&state, player, numActions, deckCount);
-        assert(result == 1);
-	}
-
-	printf("Tests Complete\n");
-
-	return 0;
-}
-/*
-int adventurer_card(struct gameState *state, int *currentPlayer, int *drawntreasure, int *cardDrawn, int *temphand, int *z){
-
-    while(*drawntreasure<2){
-  if (state->deckCount[*currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-    shuffle(*currentPlayer, state);
-  }
-  drawCard(*currentPlayer, state);
-  *cardDrawn = state->hand[*currentPlayer][state->handCount[*currentPlayer]-1];//top card of hand is most recently drawn card.
-  if (*cardDrawn == copper || *cardDrawn == silver || *cardDrawn == gold)
-    //*drawntreasure++;
-    *drawntreasure=drawntreasure+2;
-  else{
-    temphand[*z]=*cardDrawn;
-    state->handCount[*currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-    *z++;
-  }
-    }
-    while(*z-1>=0){
-  state->discard[*currentPlayer][state->discardCount[*currentPlayer]++]=temphand[*z-1]; // discard all cards in play that have been drawn
-  *z=*z-1;
+    printf("------Adventurer Card Test------\n");
+        printf("%d", j);
+        result = testAdventurer(&G, handPos);
+        if(result == 0){
+            printf("Adventurer: PASS tests\n");
+        }
+        else{
+            printf("Adventurer: FAIL test\n");
+        }
     }
     return 0;
 }
-*/
+
+int testAdventurer(struct gameState* before, int handPos){
+    int currentPlayer = whoseTurn(before);
+    int beforeAdventurerCardCount = before->handCount[currentPlayer];
+    int i, x, after;
+    int flag = 0;
+    int beforeAdventurerCoinCount = 0;
+    for(i = 0; i < beforeAdventurerCardCount; i++){
+        x = before->hand[currentPlayer][i];
+        if (x == copper || x == silver || x == gold){
+            beforeAdventurerCoinCount++;
+        }
+    }
+    after = cardEffect(adventurer, 0, 0, 0, before, handPos, 0); // run cardEffect
+    discardCard(handPos, currentPlayer, before, 0);
+    printf("    Card count in hand before: %d\n", beforeAdventurerCardCount);
+    printf("    Card count in hand after: %d\n", before->handCount[currentPlayer]);
+    printf("    Expected count: %d\n", beforeAdventurerCardCount + 1);
+    if((beforeAdventurerCardCount + 1)== before->handCount[currentPlayer]){
+        printf("        SUCCESS: card count is correct\n");
+    }
+    else{
+        printf("        FAIL: card count is incorrect\n");
+        flag = 1;
+    }
+    int j;
+    int afterAdventurerCoinCount = 0;
+    printf("    Treasure count before Adventurer:%d", beforeAdventurerCoinCount);
+    for(j = 0; j < before->handCount[currentPlayer]; j++){
+        x = before->hand[currentPlayer][i];
+        if (x == copper || x == silver || x == gold){
+            afterAdventurerCoinCount++;
+        }
+    }
+    printf("    Treasure count before Adventurer: %d\n", beforeAdventurerCoinCount);
+    printf("    Treasure count after Adventurer: %d\n", afterAdventurerCoinCount);
+    printf("    Expected count: %d\n", beforeAdventurerCoinCount + 2);
+    if((beforeAdventurerCoinCount + 2)== afterAdventurerCoinCount){
+        printf("        SUCCESS: treasure count is correct\n");
+    }
+    else{
+        printf("        FAIL: treasure count is incorrect\n");
+        flag = 1;
+    }
+
+    int costAdventurer = getCost(adventurer);
+    printf("    Cost of Card\n");
+    printf("        Actual cost: %d\n", costAdventurer);
+    printf("        Expected cost: 6\n");
+    if(costAdventurer == 6){
+        printf("            PASS: cost of Adventurer card is correct\n");
+    }
+    else{
+        printf("            FAIL: cost of Adventurer card is incorrect\n");
+        flag = 1;
+    }
+
+    return flag;
+}
